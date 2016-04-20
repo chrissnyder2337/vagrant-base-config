a306fb33f113ee1de3fd239a1d9478af7ca4c0ea
Chris Snyder <chris@chrissnyder.org>
2016-04-20 01:58:42 -0400

    Update setup.sh

--- provision/setup.sh
+++ provision/setup.sh
@@ -12,15 +12,7 @@ apt-get install nginx -y > /dev/null
 
 # PHP
 echo "Updating PHP repository"
-apt-get install python-software-properties build-essential -y > /dev/null
-add-apt-repository ppa:ondrej/php5 -y > /dev/null
-apt-get update > /dev/null
-
-echo "Installing PHP"
-apt-get install php5-common php5-dev php5-cli php5-fpm -y > /dev/null
-
-echo "Installing PHP extensions"
-apt-get install curl php5-curl php5-gd php5-mcrypt php5-mysql -y > /dev/null
+# TODO ADD
 
 # MySQL 
 echo "Preparing MySQL"
